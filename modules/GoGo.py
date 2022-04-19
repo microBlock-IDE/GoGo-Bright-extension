9a11a8ffae6eb0e3055985f1089be43f00dc48c1
Max <sonthaya.nt@hotmail.com>
2022-04-20 04:20:01 +0700

    Update GoGo.py

--- modules/GoGo.py
+++ modules/GoGo.py
@@ -43,72 +43,75 @@ CMD_MOTOR_PWR = 6
 def _limit(v, v_max, v_min=0):
     return max(min(v, v_max), v_min)
 
-def readInput(port):
-    output_port = _limit(output_port - 1, 3)
-    x = i2c1.readfrom_mem(ADDR, 1 + (output_port * 2), 1)[0]
-    y = i2c1.readfrom_mem(ADDR, 1 + (output_port * 2) + 1, 1)[0]
-    return (x << 8 | y)
-
-def wireWriteDataByte(cmd, param):
+def _writeCmd(cmd, param):
     i2c1.writeto(ADDR, bytearray([ CATEGORY_CMD, cmd ] + param))
+
+def _bitSet(v, b):
+    return v | (1 << b)
+
+def readInput(port):
+    port = _limit(port - 1, 3)
+    x = i2c1.readfrom_mem(ADDR, 1 + (port * 2), 1)[0]
+    y = i2c1.readfrom_mem(ADDR, 1 + (port * 2) + 1, 1)[0]
+    return (x << 8 | y)
 
 def talkToOutput(output_port):
     motorBits = 0
     if 'a' in output_port:
-        motorBits = motorBits | (1 << 0)
+        motorBits = _bitSet(motorBits, 0)
     if 'b' in output_port:
-        motorBits = motorBits | (1 << 1)
+        motorBits = _bitSet(motorBits, 1)
     if 'c' in output_port:
-        motorBits = motorBits | (1 << 2)
+        motorBits = _bitSet(motorBits, 2)
     if 'd' in output_port:
-        motorBits = motorBits | (1 << 3)
-    i2c1.writeto(ADDR, bytearray([ 0, CMD_MOTOR_ACTIVE, motorBits ]))
+        motorBits = _bitSet(motorBits, 3)
+    _writeCmd(CMD_MOTOR_ACTIVE, [ motorBits ])
 
 def setOutputPower(power):
     power = _limit(power, 100)
-    i2c1.writeto(ADDR, bytearray([ 0, CMD_MOTOR_PWR, 0, 0, power ]))
+    _writeCmd(CMD_MOTOR_PWR, [ 0, 0, power ])
 
 def turnOutputOn():
-    i2c1.writeto(ADDR, bytearray([ 0, CMD_MOTOR_ONOFF, 0, 1 ]))
+    _writeCmd(CMD_MOTOR_ONOFF, [ 0, 1 ])
 
 def turnOutputOff():
-    i2c1.writeto(ADDR, bytearray([ 0, CMD_MOTOR_ONOFF, 0, 0 ]))
+    _writeCmd(CMD_MOTOR_ONOFF, [ 0, 0 ])
 
 def turnOutputCW():
-    i2c1.writeto(ADDR, bytearray([ 0, CMD_MOTOR_DIR, 0, 1 ]))
+    _writeCmd(CMD_MOTOR_DIR, [ 0, 1 ])
 
 def turnOutputCCW():
-    i2c1.writeto(ADDR, bytearray([ 0, CMD_MOTOR_DIR, 0, 0 ]))
+    _writeCmd(CMD_MOTOR_DIR, [ 0, 0 ])
 
 def reverseOutputDirection():
-    i2c1.writeto(ADDR, bytearray([ 0, CMD_MOTOR_RD, 0 ]))
+    _writeCmd(CMD_MOTOR_RD, [ 0 ])
 
 def talkToServo(servo_port):
     servoBits = 0
-    if 'a' in servo_port:
-        servoBits = servoBits | (1 << 0)
-    if 'b' in servo_port:
-        servoBits = servoBits | (1 << 1)
-    if 'c' in servo_port:
-        servoBits = servoBits | (1 << 2)
-    if 'd' in servo_port:
-        servoBits = servoBits | (1 << 3)
-    i2c1.writeto(ADDR, bytearray([ 0, CMD_SERVO_ACTIVE, servoBits ]))
+    if '1' in servo_port:
+        servoBits = _bitSet(servoBits, 0)
+    if '2' in servo_port:
+        servoBits = _bitSet(servoBits, 1)
+    if '3' in servo_port:
+        servoBits = _bitSet(servoBits, 2)
+    if '4' in servo_port:
+        servoBits = _bitSet(servoBits, 3)
+    print("bit: {}".format(hex(servoBits)))
+    _writeCmd(CMD_SERVO_ACTIVE, [ servoBits ])
 
 def setServoAngle(head_angle):
-    head_angle = _limit(head_angle, 100)
-    i2c1.writeto(ADDR, bytearray([ 0, CMD_SERVO_SET_ANGLE, 0, 0, head_angle ]))
+    head_angle = _limit(head_angle, 180)
+    _writeCmd(CMD_SERVO_SET_ANGLE, [ 0, 0, head_angle ])
 
 def setServoDuty(port, percentage):
     port = _limit(port - 1, 3)
     percentage = _limit(percentage, 100)
-    i2c1.writeto(ADDR, bytearray([ 0, CMD_SERVO_SET_DUTY, 1 << port, 0, percentage ]))
+    _writeCmd(CMD_SERVO_SET_DUTY, [ 1 << port, 0, percentage ])
 
 def turnServoCW(cw_angle):
     cw_angle = _limit(cw_angle, 180)
-    i2c1.writeto(ADDR, bytearray([ 0, CMD_SERVO_CW, 0, 0, cw_angle ]))
+    _writeCmd(CMD_SERVO_CW, [ 0, 0, cw_angle ])
 
 def turnServoCCW(ccw_angle):
     ccw_angle = _limit(ccw_angle, 180)
-    i2c1.writeto(ADDR, bytearray([ 0, CMD_SERVO_CCW, 0, 0, ccw_angle ]))
-
+    _writeCmd(CMD_SERVO_CCW, [ 0, 0, ccw_angle ])
