525d2598c91d341a9e9b7f137f538893a976dcdf
Max <sonthaya.nt@hotmail.com>
2022-04-20 04:24:05 +0700

    Add import GoGo module

--- blocks/generators.js
+++ blocks/generators.js
@@ -1,6 +1,8 @@
 // ********************* Sensor ********************* //
 
 Blockly.Python['gogo_bright_input_sensor'] = function(block) {
+	Blockly.Python.definitions_['import_GoGO'] = 'import GoGO';
+
 	return [
 		'GoGo.readInput(' + block.getFieldValue('SENSOR') + ')',
 		Blockly.Python.ORDER_ATOMIC
@@ -8,11 +10,13 @@ Blockly.Python['gogo_bright_input_sensor'] = function(block) {
 };
 
 Blockly.Python['gogo_bright_input_sensor_color_is'] = function (block) {
+	Blockly.Python.definitions_['import_GoGO'] = 'import GoGO';
+
     // var dropdownSensor = this.getFieldValue('SENSOR')
 	var dropdownColor = this.getFieldValue('COLOR');
 	var sensorValue = 'GoGo.readInput(' + block.getFieldValue('SENSOR') + ')';
-  var code = '';
-  switch (dropdownColor) {
+	var code = '';
+	switch (dropdownColor) {
       case 'green':
         code = '(' + sensorValue + ' < 300)'
         break
@@ -32,6 +36,8 @@ Blockly.Python['gogo_bright_input_sensor_color_is'] = function (block) {
 // ********************* Motor ********************* //
 
 Blockly.Python['gogo_bright_action_motor'] = function () {
+	Blockly.Python.definitions_['import_GoGO'] = 'import GoGO';
+
 	var checkboxA = this.getFieldValue('a');
 	var checkboxB = this.getFieldValue('b');
 	var checkboxC = this.getFieldValue('c');
@@ -47,24 +53,32 @@ Blockly.Python['gogo_bright_action_motor'] = function () {
 }
 
 Blockly.Python['gogo_bright_motor_action_turn'] = function () {
+	Blockly.Python.definitions_['import_GoGO'] = 'import GoGO';
+
 	var dropdownTurn = this.getFieldValue('turn');
 	var code = 'GoGo.' + (dropdownTurn == 'on' ? 'turnOutputOn' : 'turnOutputOff') + '()\n';
 	return code;
 }
 
 Blockly.Python['gogo_bright_motor_action_power'] = function (block) {
+	Blockly.Python.definitions_['import_GoGO'] = 'import GoGO';
+
 	var valuePower = Blockly.Python.valueToCode(block, 'power', Blockly.Python.ORDER_NONE);
 	var code = `GoGo.setOutputPower(${valuePower})\n`;
 	return code;
 }
 
 Blockly.Python['gogo_bright_motor_action_cw'] = function () {
+	Blockly.Python.definitions_['import_GoGO'] = 'import GoGO';
+
 	var dropdownThisway = this.getFieldValue('clockwise');
 	var code = 'GoGo.' + (dropdownThisway == 'cw' ? 'turnOutputCW' : 'turnOutputCCW') + '()\n';
 	return code;
 }
 
 Blockly.Python['gogo_bright_motor_action_rd'] = function () {
+	Blockly.Python.definitions_['import_GoGO'] = 'import GoGO';
+
 	var code = 'GoGo.reverseOutputDirection()\n';
 	return code;
 }
@@ -72,6 +86,8 @@ Blockly.Python['gogo_bright_motor_action_rd'] = function () {
 // ********************* Servo ********************* //
 
 Blockly.Python['gogo_bright_action_servo'] = function () {
+	Blockly.Python.definitions_['import_GoGO'] = 'import GoGO';
+
 	var checkboxA = this.getFieldValue('a');
 	var checkboxB = this.getFieldValue('b');
 	var checkboxC = this.getFieldValue('c');
@@ -87,18 +103,24 @@ Blockly.Python['gogo_bright_action_servo'] = function () {
 }
 
 Blockly.Python['gogo_bright_servo_seth'] = function (block) {
+	Blockly.Python.definitions_['import_GoGO'] = 'import GoGO';
+
 	var valueHeading = Blockly.Python.valueToCode(block, 'heading', Blockly.Python.ORDER_NONE);
 	var code = `GoGo.setServoAngle(${valueHeading});\n`;
 	return code;
 }
 
 Blockly.Python['gogo_bright_servo_turn_cw'] = function (block) {
+	Blockly.Python.definitions_['import_GoGO'] = 'import GoGO';
+
 	var valueHeading = Blockly.Python.valueToCode(block, 'heading', Blockly.Python.ORDER_NONE);
 	var code = `GoGo.turnServoCW(${valueHeading});\n`;
 	return code;
 }
 
 Blockly.Python['gogo_bright_servo_turn_ccw'] = function (block) {
+	Blockly.Python.definitions_['import_GoGO'] = 'import GoGO';
+	
 	var valueHeading = Blockly.Python.valueToCode(block, 'heading', Blockly.Python.ORDER_NONE);
 	var code = `GoGo.turnServoCCW(${valueHeading});\n`;
 	return code;
@@ -107,6 +129,8 @@ Blockly.Python['gogo_bright_servo_turn_ccw'] = function (block) {
 // ********************* Other : Relay ********************* //
 
 Blockly.Python['gogo_bright_action_relay'] = function(block) {
+	Blockly.Python.definitions_['import_GoGO'] = 'import GoGO';
+	
 	var code = `GoGo.setServoDuty(${block.getFieldValue('PORT')}, ${(block.getFieldValue('turn')=='on') ? 0 : 100});\n`;
 	return code;
 };
